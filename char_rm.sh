4f2038c0b727eea2919e193481e649cec119eab8
DEVFLOATER86-XL <devfloater86-xl@MacBook-Pro-4.local>
2017-04-21 11:22:26 -0400

    changing names one directory at a time to avoid rename conflicts

--- char_rm.sh
+++ char_rm.sh
@@ -3,20 +3,23 @@
 # for backslash use \\\\
 # for forward slash use \:
 
-for var in "$@"
-do
-	find . -name "*$var*" | while read -r f; do echo mv "$f" "${f//$var/}"; done
-done
+dir_depth() {
+  cd "$1"
+  maxdepth=0
+  for d in */.; do
+    [ -d "$d" ] || continue
+    depth=`dir_depth "$d"`
+    maxdepth=$(($depth > $maxdepth ? $depth : $maxdepth))
+  done
+  echo $((1 + $maxdepth))
+}
 
-echo "Do you wish to perform the renames?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) 
-			for var in "$@"
-			do
-				find . -name "*$var*" | while read -r f; do mv "$f" "${f//$var/}"; done;
-			done
-		break;;
-        No ) exit;;
-    esac
+max_depth=$(dir_depth);
+
+for current_depth in `seq 1 $max_depth`;
+do	
+	for var in "$@"
+	do
+		find . -name "*$var*" -maxdepth $current_depth | while read -r f; do mv "$f" "${f//$var/}"; done;
+	done
 done
